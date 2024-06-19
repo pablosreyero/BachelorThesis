c46e147fb81c59258523b0b8ff290b712ad350ff
Pablo Suárez <pablosreyero@gmail.com>
2024-06-19 20:05:55 +0200

    Deleted some unused libraries

--- Codes/funciones.py
+++ Codes/funciones.py
@@ -1,24 +1,11 @@
-from ctypes import sizeof
 import pandas as pd
 import numpy as np 
 import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-from PIL import Image
 import torch
 import torchvision
 from torchvision.io import read_image
 from torchvision.utils import draw_bounding_boxes
-import torchvision.transforms.functional as fn
-import math as mt
 import os
-from os import listdir
-import tensorflow as tf
-import pathlib as Path
-import imghdr
-from collections import defaultdict
-from copy import deepcopy
-import json
-import sys
 import cv2
 from keras.layers import Input
 from keras.models import Model
