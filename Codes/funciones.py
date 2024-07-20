20af16712e51163a3721c3ea7b88ba8654f35bd1
Pablo Suárez <pablosreyero@gmail.com>
2024-07-20 18:33:15 +0200

    Added a while True, so that I get all the debug images

--- Codes/funciones.py
+++ Codes/funciones.py
@@ -116,7 +116,7 @@ def main(C,output_weight_path,record_path,base_weight_path,config_output_filenam
     all_img_data = train_lst
 
     #-------------HERE WE SHUEFFLE THE IMAGES WITH A RANDOM SEED-------------#
-    random.seed(5)
+    random.seed(10) # the seed was 5
     random.shuffle(all_img_data)
 
     #Now we create all anchors
@@ -129,144 +129,144 @@ def main(C,output_weight_path,record_path,base_weight_path,config_output_filenam
     C.img_channel_mean = channel_means
     if C.verbose: print(f"Channel_mean: {C.img_channel_mean}")
 
-    train_data_gen = get_anchor_gt(all_img_data,
-                                   C,
-                                   utils.get_img_output_length,
-                                   mode='train')
+    while True:
+        train_data_gen = get_anchor_gt(all_img_data,
+                                    C,
+                                    utils.get_img_output_length,
+                                    mode='train')
 
-    X, Y, image_data, debug_img, debug_num_pos = next(train_data_gen)
+        X, Y, image_data, debug_img, debug_num_pos = next(train_data_gen)
 
+        if C.verbose: print(f'Esto es el image data: {image_data}')
 
-    if C.verbose: print(f'Esto es el image data: {image_data}')
-
-    #Aqui ya se empieza a pasar los datos de entreno
-    if C.verbose:
-        print('Original image: height=%d width=%d'%(image_data[1]['h'],
-                                                    image_data[1]['w']))
-        print('Resized image:  height=%d width=%d C.im_size=%d'%(X.shape[1],
-                                                                 X.shape[2],
-                                                                 C.im_size))
-        print('Feature map size: height=%d width=%d C.rpn_stride=%d'%(Y[0].shape[1],
-                                                                      Y[0].shape[2],
-                                                                      C.rpn_stride))
-        print(X.shape)
-        print(str(len(Y))+" includes 'y_rpn_cls' and 'y_rpn_regr'")
-        print('Shape of y_rpn_cls {}'.format(Y[0].shape))
-        print('Shape of y_rpn_regr {}'.format(Y[1].shape))
-        print(image_data)
-        print('Number of positive anchors for this image: %d' % (debug_num_pos))
-
-    if debug_num_pos==0:
-        print("PRINTING the DEBUG image")
-        gt_x1, gt_x2 = image_data[1]['boxes'][0][0]*(X.shape[2]/image_data[1]['h']), image_data[1]['boxes'][0][2]*(X.shape[2]/image_data[1]['h'])
-        gt_y1, gt_y2 = image_data[1]['boxes'][0][1]*(X.shape[1]/image_data[1]['w']), image_data[1]['boxes'][0][3]*(X.shape[1]/image_data[1]['w'])
-        gt_x1,gt_y1, gt_x2, gt_y2 = int(gt_x1),int(gt_y1),int(gt_x2),int(gt_y2)
-
-        img = debug_img.copy()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        color = (0, 255, 0)
-
-        cv2.putText(img,
-                    'gt bbox',
-                    (gt_x1, gt_y1-5),
-                    cv2.FONT_HERSHEY_DUPLEX,
-                    0.7,
-                    color,
-                    1)
-        cv2.rectangle(img,
-                      (gt_x1, gt_y1),
-                      (gt_x2, gt_y2),
-                      color,
-                      2)
-        cv2.circle(img,
-                   (int((gt_x1+gt_x2)/2),int((gt_y1+gt_y2)/2)),
-                   3,
-                   color,
-                   -1)
-
-        plt.grid()
-        plt.imshow(img)
-        plt.show()
-
-    else:
-        print(f"PRINTING the DEBUG image (else statement) & debug_num_pos = {debug_num_pos}")
-        cls = Y[0][0]
-        pos_cls = np.where(cls==1)
-        if C.verbose: print(pos_cls)
-        regr = Y[1][0]
-        pos_regr = np.where(regr==1)
+        #Aqui ya se empieza a pasar los datos de entreno
         if C.verbose:
-            print(pos_regr)
-            print('y_rpn_cls for possible pos anchor: {}'.format(cls[pos_cls[0][0],pos_cls[1][0],:]))
-            print('y_rpn_regr for positive anchor: {}'.format(regr[pos_regr[0][0],pos_regr[1][0],:]))
+            print('Original image: height=%d width=%d'%(image_data[1]['h'],
+                                                        image_data[1]['w']))
+            print('Resized image:  height=%d width=%d C.im_size=%d'%(X.shape[1],
+                                                                    X.shape[2],
+                                                                    C.im_size))
+            print('Feature map size: height=%d width=%d C.rpn_stride=%d'%(Y[0].shape[1],
+                                                                        Y[0].shape[2],
+                                                                        C.rpn_stride))
+            print(X.shape)
+            print(str(len(Y))+" includes 'y_rpn_cls' and 'y_rpn_regr'")
+            print('Shape of y_rpn_cls {}'.format(Y[0].shape))
+            print('Shape of y_rpn_regr {}'.format(Y[1].shape))
+            print(image_data)
+            print('Number of positive anchors for this image: %d' % (debug_num_pos))
 
-        gt_x1, gt_x2 = image_data[1]['boxes'][0]['x1']*(X.shape[2]/image_data[1]['w']), image_data[1]['boxes'][0]['x2']*(X.shape[2]/image_data[1]['w'])
-        gt_y1, gt_y2 = image_data[1]['boxes'][0]['y1']*(X.shape[1]/image_data[1]['h']), image_data[1]['boxes'][0]['y2']*(X.shape[1]/image_data[1]['h'])
-        gt_x1, gt_y1, gt_x2, gt_y2 = int(gt_x1), int(gt_y1), int(gt_x2), int(gt_y2)
+        if debug_num_pos==0:
+            print("PRINTING the DEBUG image")
+            gt_x1, gt_x2 = image_data[1]['boxes'][0][0]*(X.shape[2]/image_data[1]['h']), image_data[1]['boxes'][0][2]*(X.shape[2]/image_data[1]['h'])
+            gt_y1, gt_y2 = image_data[1]['boxes'][0][1]*(X.shape[1]/image_data[1]['w']), image_data[1]['boxes'][0][3]*(X.shape[1]/image_data[1]['w'])
+            gt_x1,gt_y1, gt_x2, gt_y2 = int(gt_x1),int(gt_y1),int(gt_x2),int(gt_y2)
 
-        img = debug_img.copy()
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        color = (0, 255, 0)
-        # cv2.putText(img, 'gt bbox', (gt_x1, gt_y1-5), cv2.FONT_HERSHEY_DUPLEX, 0.7, color, 1)
-        cv2.rectangle(img,
-                      (gt_x1, gt_y1),
-                      (gt_x2, gt_y2),
-                      color,
-                      2)
-        cv2.circle(img,
-                   (int((gt_x1+gt_x2)/2),
-                    int((gt_y1+gt_y2)/2)),
+            img = debug_img.copy()
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            color = (0, 255, 0)
+
+            cv2.putText(img,
+                        'gt bbox',
+                        (gt_x1, gt_y1-5),
+                        cv2.FONT_HERSHEY_DUPLEX,
+                        0.7,
+                        color,
+                        1)
+            cv2.rectangle(img,
+                        (gt_x1, gt_y1),
+                        (gt_x2, gt_y2),
+                        color,
+                        2)
+            cv2.circle(img,
+                    (int((gt_x1+gt_x2)/2),int((gt_y1+gt_y2)/2)),
                     3,
                     color,
                     -1)
 
-        # Add text
-        textLabel = 'gt bbox'
-        (retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,0.5,1)
-        textOrg = (gt_x1, gt_y1+5)
+            plt.grid()
+            plt.imshow(img)
+            plt.show()
 
-        cv2.rectangle(img,
-                      (textOrg[0] - 5,
-                       textOrg[1]+baseLine - 5),
-                       (textOrg[0]+retval[0] + 5,
-                        textOrg[1]-retval[1] - 5),
-                        (0, 0, 0), 2)
-        cv2.rectangle(img,
-                      (textOrg[0] - 5,
-                       textOrg[1]+baseLine - 5),
-                       (textOrg[0]+retval[0] + 5,
-                        textOrg[1]-retval[1] - 5),
-                        (255, 255, 255), -1)
-        cv2.putText(img,
-                    textLabel,
-                    textOrg,
-                    cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1)
+        else:
+            print(f"PRINTING the DEBUG image (else statement) & debug_num_pos = {debug_num_pos}")
+            cls = Y[0][0]
+            pos_cls = np.where(cls==1)
+            if C.verbose: print(pos_cls)
+            regr = Y[1][0]
+            pos_regr = np.where(regr==1)
+            if C.verbose:
+                print(pos_regr)
+                print('y_rpn_cls for possible pos anchor: {}'.format(cls[pos_cls[0][0],pos_cls[1][0],:]))
+                print('y_rpn_regr for positive anchor: {}'.format(regr[pos_regr[0][0],pos_regr[1][0],:]))
 
-        # Draw positive anchors according to the y_rpn_regr
-        for i in range(debug_num_pos):
-            color = (100+i*(155/4), 0, 100+i*(155/4))
-            idx = pos_regr[2][i*4]/4
-            anchor_size = C.anchor_box_scales[int(idx/3)]
-            anchor_ratio = C.anchor_box_ratios[2-int((idx+1)%3)]
-            center = (pos_regr[1][i*4]*C.rpn_stride,
-                      pos_regr[0][i*4]*C.rpn_stride)
+            gt_x1, gt_x2 = image_data[1]['boxes'][0]['x1']*(X.shape[2]/image_data[1]['w']), image_data[1]['boxes'][0]['x2']*(X.shape[2]/image_data[1]['w'])
+            gt_y1, gt_y2 = image_data[1]['boxes'][0]['y1']*(X.shape[1]/image_data[1]['h']), image_data[1]['boxes'][0]['y2']*(X.shape[1]/image_data[1]['h'])
+            gt_x1, gt_y1, gt_x2, gt_y2 = int(gt_x1), int(gt_y1), int(gt_x2), int(gt_y2)
 
-            print('Center position of positive anchor: ', center)
-            cv2.circle(img, center, 3, color, -1)
-            anc_w, anc_h = anchor_size*anchor_ratio[0],anchor_size*anchor_ratio[1]
+            img = debug_img.copy()
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            color = (0, 255, 0)
+            # cv2.putText(img, 'gt bbox', (gt_x1, gt_y1-5), cv2.FONT_HERSHEY_DUPLEX, 0.7, color, 1)
             cv2.rectangle(img,
-                          (center[0]-int(anc_w/2),
-                           center[1]-int(anc_h/2)),
-                           (center[0]+int(anc_w/2),
-                            center[1]+int(anc_h/2)),
-                            color,
-                            2)
-    # cv2.putText(img, 'pos anchor bbox '+str(i+1), (center[0]-int(anc_w/2), center[1]-int(anc_h/2)-5), cv2.FONT_HERSHEY_DUPLEX, 0.5, color, 1)
-    print('GREEN bboxes ground-truth. other -> positive anchors')
-    plt.figure(figsize=(8,8))
-    plt.grid()
-    plt.imshow(img)
-    plt.show()
+                        (gt_x1, gt_y1),
+                        (gt_x2, gt_y2),
+                        color,
+                        2)
+            cv2.circle(img,
+                    (int((gt_x1+gt_x2)/2),
+                        int((gt_y1+gt_y2)/2)),
+                        3,
+                        color,
+                        -1)
+
+            # Add text
+            textLabel = 'gt bbox'
+            (retval,baseLine) = cv2.getTextSize(textLabel,cv2.FONT_HERSHEY_COMPLEX,0.5,1)
+            textOrg = (gt_x1, gt_y1+5)
+
+            cv2.rectangle(img,
+                        (textOrg[0] - 5,
+                        textOrg[1]+baseLine - 5),
+                        (textOrg[0]+retval[0] + 5,
+                            textOrg[1]-retval[1] - 5),
+                            (0, 0, 0), 2)
+            cv2.rectangle(img,
+                        (textOrg[0] - 5,
+                        textOrg[1]+baseLine - 5),
+                        (textOrg[0]+retval[0] + 5,
+                            textOrg[1]-retval[1] - 5),
+                            (255, 255, 255), -1)
+            cv2.putText(img,
+                        textLabel,
+                        textOrg,
+                        cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 0), 1)
+
+            # Draw positive anchors according to the y_rpn_regr
+            for i in range(debug_num_pos):
+                color = (100+i*(155/4), 0, 100+i*(155/4))
+                idx = pos_regr[2][i*4]/4
+                anchor_size = C.anchor_box_scales[int(idx/3)]
+                anchor_ratio = C.anchor_box_ratios[2-int((idx+1)%3)]
+                center = (pos_regr[1][i*4]*C.rpn_stride,
+                        pos_regr[0][i*4]*C.rpn_stride)
+
+                print('Center position of positive anchor: ', center)
+                cv2.circle(img, center, 3, color, -1)
+                anc_w, anc_h = anchor_size*anchor_ratio[0],anchor_size*anchor_ratio[1]
+                cv2.rectangle(img,
+                            (center[0]-int(anc_w/2),
+                            center[1]-int(anc_h/2)),
+                            (center[0]+int(anc_w/2),
+                                center[1]+int(anc_h/2)),
+                                color,
+                                2)
+        # cv2.putText(img, 'pos anchor bbox '+str(i+1), (center[0]-int(anc_w/2), center[1]-int(anc_h/2)-5), cv2.FONT_HERSHEY_DUPLEX, 0.5, color, 1)
+        print('GREEN bboxes ground-truth. other -> positive anchors')
+        plt.figure(figsize=(8,8))
+        plt.grid()
+        plt.imshow(img)
+        plt.show()
 
     '''
     #-------------------Here we're building the model-------------------#
