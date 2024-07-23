5ca57d8a2c0c806c2a66a369c85468097cfee81a
Pablo Suárez <pablosreyero@gmail.com>
2024-07-23 21:24:11 +0200

    Getting started with COCO.py

---
+++ Codes/COCO.py
@@ -0,0 +1,8 @@
+'''In this file we will download and install the Python COCO tools in order to
+train our RPN model so that it can easily detect welding defects in the future
+'''
+
+import os 
+import time 
+import numpy as np
+
